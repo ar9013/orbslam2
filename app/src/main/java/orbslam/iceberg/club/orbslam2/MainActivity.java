206fc76c90c8277f3c9855f0cdb1a62083d64e47
ar9013 <ar9013@gmail.com>
2018-01-26 12:44:10 +0800

    show red text

--- app/src/main/java/orbslam/iceberg/club/orbslam2/MainActivity.java
+++ app/src/main/java/orbslam/iceberg/club/orbslam2/MainActivity.java
@@ -1,6 +1,7 @@
 package orbslam.iceberg.club.orbslam2;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,11 +12,15 @@ public class MainActivity extends Activity {
         System.loadLibrary("native-lib");
     }
 
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = (TextView)findViewById(R.id.tv01);
+        tv.setTextColor(Color.RED);
 
     }
 
